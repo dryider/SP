39ad7a9a1f819f46c2773fba6924218bec470b87
dryider <85456965+dryider@users.noreply.github.com>
2022-06-21 20:38:23 +0300

    02_БаженовД.С._205(П.Р.2)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,21 +12,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
-namespace Lab1
+namespace Lab2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        
-        public ThreadControl thread1 = new ThreadControl();
-        public ThreadControl thread2 = new ThreadControl();
-       
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         public void ProgressBar1Update(Array sender, ArrayEventArgs e)
@@ -42,22 +39,24 @@ namespace Lab1
 
         private void Button_Click_Stop1(object sender, RoutedEventArgs e)
         {
-            thread1.Stop();
+            
         }
         private void Button_Click_Stop2(object sender, RoutedEventArgs e)
         {
-            thread2.Stop();
+            
         }
 
+       
         private void Button_Click_Start(object sender, RoutedEventArgs e)
         {
             Array arr1 = new Array(10000, ProgressBar1Update);
-            Array arr2 = new Array(10000, ProgressBar2Update);
+            Array arr2 = new Array(15000, ProgressBar2Update);
             arr1.PercantageChange += ProgressBar1Update;
             arr2.PercantageChange += ProgressBar2Update;
-            thread1.Start(arr1.Sort);
-            thread2.Start(arr2.Sort_Desc);
+            arr1.SortAsync();
+            arr2.SortDescAsync();
 
         }
     }
+    
 }
